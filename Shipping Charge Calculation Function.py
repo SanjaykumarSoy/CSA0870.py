fd4ce033818e901719cb052fe115c68213b8ee6f
SanjaykumarSoy <157505017+SanjaykumarSoy@users.noreply.github.com>
2024-05-13 11:40:39 +0530

    Update Shipping Charge Calculation Function.py

--- Shipping Charge Calculation Function.py
+++ Shipping Charge Calculation Function.py
@@ -4,9 +4,9 @@ def calculate_shipping_charge(num_items):
     else:
         return 750.00 + (num_items - 1) * 200.00
 
-# Read the number of items purchased from the user
+
 num_items = int(input("Enter the number of items purchased: "))
 
-# Calculate and display the shipping charge
+
 shipping_charge = calculate_shipping_charge(num_items)
 print(f"Shipping charge for {num_items} items: ${shipping_charge:.2f}")
